230e3400b268b324eb45f16a9b9a2e4a56313433
cyh <756085049@qq.com>
2020-12-03 23:41:24 +0800

    滑动窗口239-滑动窗口最大值

--- 91Day/array/maxSlidingWindow.js
+++ 91Day/array/maxSlidingWindow.js
@@ -5,5 +5,22 @@
  * @return {number[]}
  */
 var maxSlidingWindow = function(nums, k) {
-
+    let tmp = [], res = [], max = -Number.MAX_VALUE;
+    for (let i = 0,j = i + k; i < nums.length - k, j < nums.length; i++, j++) {
+        tmp = nums.slice(i, j);
+        let tmp_max = Math.max.apply(Math, tmp);
+        max = Math.max(max, tmp_max);
+        tmp = [];
+        res.push(max);
+    }
+    for (let i = nums.length - k; i < nums.length; i++) {
+        tmp.push(nums[i]);
+    }
+    let last_max = Math.max.apply(Math, tmp);
+    res.push(last_max);
+    return res;
 };
+
+
+console.log(maxSlidingWindow([1,3,-1,-3,5,3,6,7], 3));
+console.log(maxSlidingWindow([1,-1], 1));
