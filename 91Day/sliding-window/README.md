276a684b193b08ed2709564000d5e40fcd13f239
cyh <756085049@qq.com>
2021-01-07 23:31:34 +0800

    滑动窗口1456-定长子串中元音的最大数目

---
+++ 91Day/sliding-window/README.md
@@ -0,0 +1,26 @@
+## 滑动窗口
+### [1456. 定长子串中元音的最大数目](https://leetcode-cn.com/problems/maximum-number-of-vowels-in-a-substring-of-given-length/)
+#### 思路
+滑动窗口判断左右窗口元素是否为元音字母，并记录
+#### 代码
+```js
+var maxVowels = function(s, k) {
+    const vowels = {a: true, e: true, i: true, o: true, u: true
+    }
+    let i = 0, count = 0, max = 0;
+    while (i < s.length) {
+        if (i < k) {
+            if (vowels[s[i]]) count++;
+        } else {
+            if (vowels[s[i]]) count++;
+            if (vowels[s[i]]) count--;
+        }
+        i++;
+        max = Math.max(max, count);
+    }
+    return max;
+};
+```
+#### 复杂度
+时间复杂度：O(n)
+空间复杂度：O(1)
---
+++ 91Day/sliding-window/maxVowels.js
@@ -0,0 +1,42 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var maxVowels = function(s, k) {
+    let vowels = ["a", "e", "i", "o", "u"];
+    vowels = new Set(vowels);
+    let count = 0, left = 0, right = 0;
+    while (right < k) {
+        vowels.has(s[right]) && count++;
+        right++;
+    }
+    let max = count;
+    while (right < s.length) {
+        vowels.has(s[right]) && count++;
+        vowels.has(s[left]) && count--;
+        left++;
+        right++;
+        max = Math.max(max, count);
+    }
+    return max;
+};
+
+var maxVowels1 = function(s, k) {
+    const vowels = {a: true, e: true, i: true, o: true, u: true
+    }
+    let i = 0, count = 0, max = 0;
+    while (i < s.length) {
+        if (i < k) {
+            if (vowels[s[i]]) count++;
+        } else {
+            if (vowels[s[i]]) count++;
+            if (vowels[s[i]]) count--;
+        }
+        i++;
+        max = Math.max(max, count);
+    }
+    return max;
+};
+
+console.log(maxVowels1("rhythms", 3));
