92efbe6321223216717e64d98062340223376047
cyh756085049 <756085049@qq.com>
2023-06-23 11:18:59 +0800

    只出现一次的数字

---
+++ hot100/other/P136_singleNumber.js
@@ -0,0 +1,26 @@
+/**
+ * 136. 只出现一次的数字
+ * 给你一个 非空 整数数组 nums ，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+ * 你必须设计并实现线性时间复杂度的算法来解决此问题，且该算法只使用常量额外空间。
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    let res = 0;
+    for (let num of nums) {
+        res ^= num;
+    }
+
+    return res;
+};
+
+/**
+ * 输入：nums = [2,2,1]
+ * 输出：1
+ *
+ * 输入：nums = [4,1,2,1,2]
+ * 输出：4
+ */
+
+const nums = [4,1,2,1,2];
+console.log(singleNumber(nums));
