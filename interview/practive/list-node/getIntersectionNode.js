44bbd2b819e37e211742dce9915940d4fb149a62
cyh756085049 <756085049@qq.com>
2023-07-07 22:14:43 +0800

    链表-两个单链表的相交结点

--- interview/practive/list-node/getIntersectionNode.js
+++ interview/practive/list-node/getIntersectionNode.js
@@ -3,6 +3,13 @@ function ListNode(val, next) {
     this.next = next === undefined ? null : next;
 }
 
+/**
+ * 160. 相交链表 https://leetcode.cn/problems/intersection-of-two-linked-lists/description/
+ * 双指针: 时间复杂度O(n) 空间复杂度O{1}
+ * @param headA
+ * @param headB
+ * @returns {*|null}
+ */
 const getIntersectionNode = (headA, headB) => {
     if (headA === null || headB === null) {
         return null;
